cfdadecae330a81b34f13b64bcc9271cd6102a6d
caoqingwa <162868787+caoqingwa@users.noreply.github.com>
2024-07-25 15:52:00 +0800

    Add files via upload

---
+++ test_function/test_function/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+Add(int x, int y)
+{
+	int z = 0;
+	z = x + y;
+	return z;
+}
+int main()
+{
+	int num1 = 0;
+	int num2 = 0;
+	scanf_s("%d%d",&num1,&num2);
+	int sum=Add(num1, num2);
+	printf("sum=%d",sum);
+	return 0;
+}
